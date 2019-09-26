02a9e18485d1ac11a4bb8862306a1c6216daf5fe
Maojiong.Fu <fumaojiong@xiamenair.com>
2019-09-26 10:19:30 +0800

    进一步精简页面，只留下内容

--- ItHomeClear.js
+++ ItHomeClear.js
@@ -11,6 +11,15 @@
 (function () {
     'use strict';
 
+    // 当前页面的url
+    var currentURL = window.location.href;
+
+    // 新闻列表的正则表达式
+    var newsList = /list/;
+
+    // 移除新闻类型
+    $("strong,#hd,#ft").remove();
+
     // 移除右侧列表
     $("div.sidebar").remove();
 
@@ -41,7 +50,22 @@
     // 设置底部
     $("div.bx2,div.if").css("padding", 0);
 
-    // 设置主内容为适合阅读的宽度
-    $("#con").css("width", "728px");
+    // 移除顶部banner
+    $("body").css("padding-top", 0);
+
+    // 移除新闻类型的浮动
+    $(".post_list ul li").css("padding-left", "20px");
+
+    // 如果是新闻列表
+    if (newsList.test(currentURL)) {
+        // 设置主内容为适合阅读的宽度
+        $("#con").css("width", "600px");
+
+        // 分割线宽度
+        $(".post_list ul li.hr").css("width", "580px");
+    } else {
+        // 正文
+        $("#con").css("width", "728px");
+    }
 
 })();
